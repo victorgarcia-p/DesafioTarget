d9e93065b969799d25ebc5f498a599fd32918626
Victor Garcia <victorgarcia.p@hotmail.com>
2025-02-28 20:10:21 -0300

    Criando Projeto

---
+++ DesafioTarget/Desafios/Exec1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioTarget.Desafios;
+
+class Exec1
+{
+    static void Sequencia()
+    {
+        int INDICE = 13;
+        int SOMA = 0;
+        int K = 0;
+
+        while (K < INDICE)
+        {
+            K++;
+            SOMA += K;
+        }
+
+        Console.WriteLine($"Valor da Soma: {SOMA}");
+    }
+}
---
+++ DesafioTarget/Desafios/Exec2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DesafioTarget.Desafios;
+
+class Exec2
+{
+    static bool Fibonacci(int numero)
+    {
+        int a = 0;
+        int b = 1; 
+        int temp = 0;
+
+        while (a <= numero)
+        {
+            if (a == numero) return true;
+            temp = a;
+            a = b;
+            b = temp + b;
+        }
+
+        return false;
+    }
+
+    static void Main()
+    {
+        Console.Write("Informe um número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (Fibonacci(numero))
+            Console.WriteLine($"{numero} pertence à sequência de Fibonacci.");
+        else
+            Console.WriteLine($"{numero} NÃO pertence à sequência de Fibonacci.");
+    }
+}
